b81a3f1f6a602b9acdba5accd7a483c37b57d48d
isaac <ndemabekolleemmanuelisaac@gmail.com>
2021-06-30 00:07:40 +0100

    Conservation frame

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import { MatButtonModule } from '@angular/material/button'; 
 import { MatIconModule } from '@angular/material/icon'; 
+import {MatSelectModule} from '@angular/material/select'; 
+import {MatInputModule} from '@angular/material/input'; 
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import {MatTableModule} from '@angular/material/table'; 
+import {MatPaginatorModule} from '@angular/material/paginator'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './main/main.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
@@ -23,7 +31,16 @@ import { MainComponent } from './main/main.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    FormsModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
+interface Contact {
+  value: string;
+  viewValue: string;
+}
+
+interface Push{
+  value: string;
+}
+
+export interface UserConversation {
+  date: string;
+  content: string;
+  tigramme: string;
+}
 
 @Component({
   selector: 'app-main',
@@ -6,10 +24,113 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  selectedContact!: string;
+  contacts: Contact[] = [
+    {value: '17P123', viewValue: 'Isaac NDEMA'},
+    {value: '17P124', viewValue: 'Junior BEKOLLE'},
+    {value: '17P125', viewValue: 'Franck CRIPS'},
+    {value: '17P126', viewValue: 'Edouard LARIS'}
+  ];
+
+  availablePushs: Push[] = [
+    {value: ''},
+    {value: 'Node JS'},
+    {value: 'Angular'},
+    {value: 'Java'},
+    {value: 'Express'},
+    {value: 'Spring Boot'},
+    {value: 'MongoDB'},
+    {value: 'React'},
+    {value: 'Python'}
+  ];
+
+  currentContact  = {
+    name              : 'NDEMA',
+    firstname         : 'Isaac',
+    status            : 'black',
+    rappel            : new Date('6/11/2021'),
+    titre             : '',
+    email             : '',
+    tel1              : '',
+    tel2              : '',
+    mobile            : '',
+    linkedin          : '',
+    observations      : '',
+    outils            : '',
+    pushs             : ['','','','','','','',''],
+    plaquette         : {
+                          date : new Date('6/11/2021'),
+                          name : ''
+                        },
+    conversations     :[
+                        {
+                          date : '6/11/2021',
+                          content : 'Pas decisionnaire',
+                          tigramme : 'LSE'
+                        },
+                        {
+                          date : '6/11/2021',
+                          content : 'Pas decisionnaire',
+                          tigramme : 'LSE'
+                        },
+                        {
+                          date : '6/11/2021',
+                          content : 'Pas decisionnaire',
+                          tigramme : 'LSE'
+                        },
+                        {
+                          date : '6/11/2021',
+                          content : 'Pas decisionnaire',
+                          tigramme : 'LSE'
+                        }
+                      ],
+    besoins          :[
+                        {
+                          date : '',
+                          description : '',
+                          status : '',
+                          ao : {
+                                  state: '',
+                                  link : ''
+                              },
+                          cv  :[
+                                  {
+                                    name : '',
+                                    link : ''
+                                  }
+                               ],
+                          date_envoi : ''
+                        }
+
+                      ]  
+  }
+
+  pushChange(newvalue : string) : any{
+
+  }
 
   constructor() { }
-
+  
   ngOnInit(): void {
+  }
+
+
+  displayedColumns: string[] = ['date', 'content', 'tigramme'];
+  dataSourceConversation: MatTableDataSource<UserConversation> = new MatTableDataSource<UserConversation>(this.currentContact.conversations);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  newConversationText = 'New conversation';
+
+  newConversation(){
+    this.currentContact.conversations.push({
+      date : '6/30/2021',
+      content: this.newConversationText,
+      tigramme : 'LSE'
+    });
+
+    this.dataSourceConversation = new MatTableDataSource<UserConversation>(this.currentContact.conversations);
+    this.newConversationText = '';
   }
 
 }
