fcdf60ade6682aa142028ccac209059c68588550
isaac <ndemabekolleemmanuelisaac@gmail.com>
2021-07-03 08:32:47 +0100

    Add CORS support

--- src/main/java/com/constellis/test/controllers/ContactController.java
+++ src/main/java/com/constellis/test/controllers/ContactController.java
@@ -17,12 +17,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 /**
  *
  * @author Isaac
  */
 
-
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("api")
 public class ContactController {
